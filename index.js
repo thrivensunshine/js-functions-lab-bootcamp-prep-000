0bb60b3770ca44d5d35e25b4fe12b27b2ecd4b9e
DANIELLE ALBA <danib.sgh@gmail.com>
2019-01-31 18:20:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // write your code below!
 
 function happyHolidays(){
-  
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name){
-  
+  return `Happy holidays, ${name}!`;
 }
 
-function happyHolidayTo(name1, name2){
-  
+function happyHolidayTo(name, holiday){
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`;
 }
 
